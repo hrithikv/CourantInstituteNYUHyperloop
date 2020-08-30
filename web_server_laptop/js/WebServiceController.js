cb21c90413da1efaec7e2dc9b0e56b65f96ccccc
hrithikv <35966198+hrithikv@users.noreply.github.com>
2020-08-29 23:56:31 -0400

    add bash script to easily & quickly start app

---
+++ web_server_laptop/js/WebServiceController.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const axios = require('axios');
+
+function WebServiceController(baseUrl) {
+  this.wsTempUrl = `${baseUrl}/temp`;
+  this.wsDistUrl = `${baseUrl}/dist`;
+  this.wsSpeedUrl = `${baseUrl}/speed`;
+  this.wsIPAddr = `${baseUrl}/ipAddr`;
+}
+
+module.exports = WebServiceController;
+
+WebServiceController.prototype.getTemp = async function (sensorId) {
+  try {
+    const url = this.wsTempUrl + '/' + `${sensorId}`;
+    const response = await axios.get(url);
+
+    return response.data;
+
+  }
+  catch (err) {
+    console.error(err);
+    throw (err.response && err.response.data) ? err.response.data : err;
+  }
+};
+
+WebServiceController.prototype.getDist = async function (sensorId) {
+  try {
+    const url = this.wsDistUrl + '/' + `${sensorId}`;
+    const response = await axios.get(url);
+
+    return response.data;
+
+  }
+  catch (err) {
+    console.error(err);
+    throw (err.response && err.response.data) ? err.response.data : err;
+  }
+};
+
+WebServiceController.prototype.getSpeed = async function (sensorId) {
+  try {
+    const url = this.wsSpeedUrl + '/' + `${sensorId}`;
+    const response = await axios.get(url);
+
+    return response.data;
+
+  }
+  catch (err) {
+    console.error(err);
+    throw (err.response && err.response.data) ? err.response.data : err;
+  }
+};
